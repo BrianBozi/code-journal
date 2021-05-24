0717eeede73495840e9ec00a48c91066c51323ab
Brian Bozigian <bbozigian@outlook.com>
2021-05-24 15:52:43 -0700

    completed step 5

--- js/data.js
+++ js/data.js
@@ -6,3 +6,11 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var $placeHolder = document.querySelector('img');
+var $newImage = document.querySelector('input[type="url"');
+
+$newImage.addEventListener('input', function (event) {
+  $placeHolder.setAttribute('src', event.target.value);
+
+});
