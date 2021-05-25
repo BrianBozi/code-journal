473409b4814bc6614c0badb174b021ef17f22546
Brian Bozigian <bbozigian@outlook.com>
2021-05-24 20:24:55 -0700

    transfered logic from data js to main js and fixed css quirks

--- js/data.js
+++ js/data.js
@@ -17,29 +17,3 @@ window.addEventListener('beforeunload', function (event) {
   var postingsJSON = JSON.stringify(data);
   localStorage.setItem('javascript-local-storage', postingsJSON);
 });
-
-var $placeHolder = document.querySelector('img');
-var $newImage = document.querySelector('input[type="url"');
-var $newPost = document.querySelector('#new-post');
-
-$newImage.addEventListener('input', function (event) {
-  $placeHolder.setAttribute('src', event.target.value);
-});
-
-$newPost.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  var post = {
-    title: $newPost.postTitle.value,
-    image: $newPost.photoURL.value,
-    comment: $newPost.postComment.value,
-    entryId: data.nextEntryId
-  };
-
-  data.nextEntryId++;
-  data.entries.push(post);
-
-  $placeHolder.setAttribute('src', './images/placeholder-image-square.jpg');
-  document.querySelector('form').reset();
-
-});
